1796f11c61f773dd84c010f4747398b4d1025a6f
Michael Chen <mh91chen@gmail.com>
2021-02-06 23:07:20 -0500

    on_message wip

--- bot.py
+++ bot.py
@@ -8,4 +8,12 @@ client = discord.Client()
 async def on_ready():
     print("Bot is ready")
 
+
+@client.event
+async def on_message(msg):
+    if msg.author.bot:
+        return
+    await msg.channel.send(f"hello {msg.author.id}: ur msg: {msg.content}")
+
+
 client.run(os.environ["DISCORD_TOKEN"])
