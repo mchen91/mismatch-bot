750fcbf70fdf67a1371fcb419e1d39fd4a5d4b67
Michael Chen <mh91chen@gmail.com>
2021-02-13 16:03:20 -0500

    try migration autogenerate

--- models.py
+++ models.py
@@ -2,14 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.engine.url import URL
 from sqlalchemy.ext.declarative import declarative_base
 
-import settings
-
-
 Base = declarative_base()
-
-def db_connect():
-    return create_engine(URL(**settings.DATABASE))
-
 
 def create_tables(engine):
     DeclarativeBase.metadata.create_all(engine)
