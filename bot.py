4b40b4ea97b89a24225e212a05732f1b99d3f5d7
Michael Chen <mh91chen@gmail.com>
2021-02-15 01:12:13 -0500

    add update record discord command

--- bot.py
+++ bot.py
@@ -1,19 +1,30 @@
-import discord
 import os
+from discord.ext import commands
+
+MY_ID = 97904188918345728
+
+def is_me():
+    def predicate(ctx):
+        return ctx.message.author.id == MY_ID
+    return commands.check(predicate)
+
+bot = commands.Bot(command_prefix="-mm ")
 
 
-client = discord.Client()
+@bot.command()
+@is_me()
+async def update(ctx, character_name, stage_name, time_string, player_name, video_link):
+    from use_cases.records import add_record
 
-@client.event
-async def on_ready():
-    print("Bot is ready")
+    # TODO: partial targets
+    add_record(
+        character_name=character_name,
+        stage_name=stage_name,
+        player_name=player_name,
+        time_string=time_string,
+        video_link=video_link,
+    )
+    await ctx.send(f"Added {character_name}/{stage_name} - {time_string} by {player_name} at {video_link}")
 
 
-@client.event
-async def on_message(msg):
-    if msg.author.bot:
-        return
-    await msg.channel.send(f"hello {msg.author.id}: ur msg: {msg.content}")
-
-
-client.run(os.environ["DISCORD_TOKEN"])
+bot.run(os.environ["DISCORD_TOKEN"])
